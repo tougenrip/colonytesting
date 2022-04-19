a71c2a93c1b8026fed6dacb27840c59a71c4f66a
tougenrip <76558978+tougenrip@users.noreply.github.com>
2022-04-19 23:36:15 +0300

    Video Background

--- nft-minting-app-main/src/App.js
+++ nft-minting-app-main/src/App.js
@@ -198,8 +198,8 @@ function App() {
       <s.Container
         flex={1}
         ai={"center"}
-        style={{ padding: 24, backgroundColor: "var(--primary)" }}
-        image={CONFIG.SHOW_BACKGROUND ? "/config/images/bg.png" : null}
+        style={{ padding: 24, backgroundColor: "var(--primary)",backgroundRepeat: "repeat" }}
+        image={CONFIG.SHOW_BACKGROUND ? "/config/images/bg.mp4" : null}
       >
         <a href={CONFIG.MARKETPLACE_LINK}>
           <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
